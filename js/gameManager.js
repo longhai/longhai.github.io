9525edbacd4e44ef1ab9ac09e468d35897c7cb58
longhai <lelonghai@gmail.com>
2024-07-01 09:00:53 +0700

    Update gameManager.js

--- js/gameManager.js
+++ js/gameManager.js
@@ -9,8 +9,8 @@ class GameManager {
         this.totalGames = 0;
         this.searchKeyword = '';
         this.selectedLetter = 'All';
-        this.regionPriority = { 'japan': 1, 'usa': 2, 'europe': 3 };
-        this.excludedKeywords = ['bios', 'test', 'in-1', 'demo', 'beta', 'retro-bit', 'aftermarket', 'video'];
+        this.regionPriority = { 'europa': 3, 'usa': 2, 'japan': 1 };
+        this.excludedKeywords = ['bios', 'test', 'in-1', 'demo', 'beta', 'retro-bit', 'aftermarket', 'video','hack'];
     }
 
     async loadMenuContent(menuItem) {
@@ -37,52 +37,47 @@ class GameManager {
         this.headerName = xmlDoc.querySelector('header > name').textContent.trim();
         this.headerDescription = xmlDoc.querySelector('header > description').textContent.trim();
         const games = Array.from(xmlDoc.querySelectorAll('game'));
-
+    
         const uniqueGames = {};
         games.forEach(game => {
             const baseName = game.getAttribute('name').replace(/\s*\(.*?\)/g, '').toLowerCase();
-            const regionMatch = game.getAttribute('name').match(/\((japan|usa|europe)\)/i);
-            const region = regionMatch ? regionMatch[1].toLowerCase() : '';
-            const revisionMatch = game.getAttribute('name').match(/Rev\s*([A-Za-z]|\d+)/i);
+            const description = game.querySelector('description').textContent.trim();
+            const regionMatch = description.match(/\((europa|usa|japan)\)/i);
+            const region = regionMatch ? this.regionPriority[regionMatch[1].toLowerCase()] : 0;
+            const revisionMatch = description.match(/Rev\s*([A-Za-z]|\d+)/i);
             const revision = revisionMatch ? revisionMatch[1] : '';
-
             if (!(baseName in uniqueGames) || this.compareGames(uniqueGames[baseName], game, region, revision) < 0) {
                 uniqueGames[baseName] = { game, region, revision };
             }
         });
-
+    
         this.filteredGames = Object.values(uniqueGames).map(obj => obj.game);
-        this.filteredGames = this.filteredGames.filter(game => !this.excludedKeywords.some(keyword => game.getAttribute('name').toLowerCase().includes(keyword)));
+        this.filteredGames = this.filteredGames.filter(game => !this.excludedKeywords.some(keyword => game.getAttribute('name').toLowerCase().includes(keyword) || game.querySelector('description').textContent.toLowerCase().includes(keyword)));
         this.filteredGames.sort((a, b) => a.getAttribute('name').toLowerCase().localeCompare(b.getAttribute('name').toLowerCase()));
-
+    
         this.totalGames = this.filteredGames.length;
         this.updateLogo(this.headerDescription);
         this.renderGames(this.currentPage);
     }
+    
+    
 
     compareGames(existingGame, newGame, newRegion, newRevision) {
         const existingRegion = existingGame.region;
         const existingRevision = existingGame.revision;
 
-        if (this.headerName === 'Sony - PlayStation' && newRegion && existingRegion) {
-            const newRegionPriority = this.regionPriority[newRegion];
-            const existingRegionPriority = this.regionPriority[existingRegion];
-
-            if (newRegionPriority > existingRegionPriority) {
+        if (newRegion > existingRegion) {
+            return 1;
+        } else if (newRegion < existingRegion) {
+            return -1;
+        } else {
+            if (newRevision > existingRevision) {
                 return 1;
-            } else if (newRegionPriority < existingRegionPriority) {
+            } else if (newRevision < existingRevision) {
                 return -1;
             } else {
-                if (newRevision > existingRevision) {
-                    return 1;
-                } else if (newRevision < existingRevision) {
-                    return -1;
-                } else {
-                    return 0;
-                }
+                return 0;
             }
-        } else {
-            return 0;
         }
     }
 
@@ -119,25 +114,33 @@ class GameManager {
     }
 
     applySearchFilter(games) {
-        return games.filter(game => game.getAttribute('name').toLowerCase().includes(this.searchKeyword));
+        return games.filter(game => game.querySelector('description').textContent.toLowerCase().includes(this.searchKeyword.toLowerCase()));
     }
-
+    
+    
     createGameElement(game, gamesList) {
         const gameName = game.getAttribute('name');
         const gameDescription = game.querySelector('description').textContent;
+
+        const gameUrl = this.createGameUrl({ headerName: this.headerName, gameName });
 
         const gameElement = document.createElement('div');
         gameElement.classList.add('game');
         gameElement.innerHTML = `
             <img src="snap/${encodeURIComponent(this.headerDescription)}/${encodeURIComponent(gameName)}.png" 
                  alt="${gameName}" 
-                 data-gameurl="${this.createGameUrl(gameName)}"> <!-- Use createGameUrl method to generate game URL -->
+                 data-gameurl="${gameUrl}">
             <p>${gameDescription}</p>
         `;
-
         gameElement.querySelector('img').addEventListener('error', function () {
             this.src = 'https://via.placeholder.com/512x409.6?text=No+Image';
         });
+
+        // Load image from Arcade specific source if available
+        if (this.headerName === 'arcade') {
+            const arcadeImageUrl = `https://archive.org/download/2020_01_06_fbn/support/previews.zip/previews/${encodeURIComponent(gameName)}.png`;
+            this.loadArcadeImage(gameElement.querySelector('img'), arcadeImageUrl);
+        }
 
         gameElement.querySelector('img').addEventListener('click', () => {
             const gameUrl = gameElement.querySelector('img').getAttribute('data-gameurl');
@@ -147,8 +150,15 @@ class GameManager {
         gamesList.appendChild(gameElement);
     }
 
-    createGameUrl(gameName) {
-        if (this.headerName === 'Sony - PlayStation') {
+    loadArcadeImage(imgElement, imageUrl) {
+        // Set a timeout to load image, as direct fetch may not work due to CORS
+        setTimeout(() => {
+            imgElement.src = imageUrl;
+        }, 100);
+    }
+
+    createGameUrl({ headerName, gameName }) {
+        if (headerName === 'Sony - PlayStation') {
             if (gameName.includes('(Japan)')) {
                 return `https://ia801905.us.archive.org/cors_get.php?path=/10/items/chd_psx_jap/CHD-PSX-JAP/${encodeURIComponent(gameName)}.chd`;
             } else if (gameName.includes('(USA)')) {
@@ -156,12 +166,12 @@ class GameManager {
             } else if (gameName.includes('(Europe)')) {
                 return `https://ia601801.us.archive.org/cors_get.php?path=/21/items/chd_psx_eur/CHD-PSX-EUR/${encodeURIComponent(gameName)}.chd`;
             } else {
-                return `https://archive.org/download/ni-roms/roms/${encodeURIComponent(this.headerName)}.zip/${encodeURIComponent(gameName)}.zip`;
+                return `https://archive.org/download/ni-roms/roms/${encodeURIComponent(headerName)}.zip/${encodeURIComponent(gameName)}.zip`;
             }
-        } else if (this.headerName === 'arcade') {
+        } else if (headerName === 'arcade') {
             return `https://archive.org/download/2020_01_06_fbn/roms/${encodeURIComponent(this.headerName)}.zip/arcade/${encodeURIComponent(gameName)}.zip`;
         } else {
-            return `https://archive.org/download/ni-roms/roms/${encodeURIComponent(this.headerName)}.zip/${encodeURIComponent(gameName)}.zip`;
+            return `https://archive.org/download/ni-roms/roms/${encodeURIComponent(headerName)}.zip/${encodeURIComponent(gameName)}.zip`;
         }
     }
 
